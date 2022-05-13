029821c43bfdefdc6714646bb1387723f3d49ee4
sayal00 <sayal.chitrakar00@gmail.com>
2022-05-13 09:14:39 +0545

    initial

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
+
+import Navbar from "./Components/Navbar";
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="container d-flex justify-content-center">
+         <div className="my-container row">
+            <Navbar className = "col-1"/>
+         </div>
+      </div>
   );
 }
 
---
+++ src/Components/Navbar.js
@@ -0,0 +1,12 @@
+import './../css/navbar.css';
+
+function Navbar(props){
+    const col = props.className;
+    return(
+       <nav className={}>
+
+       </nav>
+    )
+}
+
+export default Navbar;
